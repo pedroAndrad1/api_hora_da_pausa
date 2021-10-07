0b0ab106efb97b8a5f1a51f91509e002f5e16492
Pedro de Andrade <dev.pedroandrade@gmail.com>
2021-10-07 00:28:54 -0300

    Configs iniciais, modelagem de Usuario e início de migration de usuários.

---
+++ src/config/database.js
@@ -0,0 +1,16 @@
+// Configs para acessar o banco no MySQL
+module.exports = {
+    host: "localhost",
+    dialect: 'mysql',
+    username: 'root',
+    password: '8016',
+    database: 'hora_da_pausa',
+    define: {
+        // Define que cada registro vai ter dois campos, um que tem a data que ele foi criado
+        // e outro  que salva a data da ultima atualizacao.
+        timestamps: true, 
+        // Define que os que os campos e nomes das tabelas vao usar _ para separar os palavras.
+        // Ex: created_at
+        underscored: true,
+    },
+};
---
+++ src/models/User.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+class Usuario extends Model {
+    //Faz a conexao com o banco de dados
+    static init(sequelize) {
+        super.init(
+            //Os campos da tabela
+            {
+                nome: DataTypes.STRING,
+                senha: DataTypes.STRING,
+                email: DataTypes.STRING,
+                islogged: DataTypes.BOOLEAN,
+                nivel: DataTypes.INTEGER,
+                xp: DataTypes.INTEGER,
+                xp_para_subir_de_nivel: DataTypes.INTEGER,
+                foto: DataTypes.BLOB,
+                doador: DataTypes.BOOLEAN,
+                quant_exercicios_feitos: DataTypes.INTEGER
+
+            }, 
+            {   //O sequelize para a conexao 
+                sequelize,
+                // Metodos que para executar operacoes no registro na criacao.
+                // Neste caso, esta hasheando o password antes de criar o registro
+                hooks: {
+                    beforeCreate: (usuario) => {
+                        const salt = bcrypt.genSaltSync();
+                        usuario.senha = bcrypt.hashSync(usuario.senha, salt);
+                    },
+                },
+            }
+        )
+    }
+
+    static associate(models) {
+          
+    }
+
+}
+
+module.exports = Usuario;
