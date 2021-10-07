6f90d161ce0076d4532f43536cf1ba75d2465ba2
Pedro de Andrade <dev.pedroandrade@gmail.com>
2021-10-07 00:55:22 -0300

    Feita a migration de usuarios

--- src/database/migrations/20211007032524-create-usuarios.js
+++ src/database/migrations/20211007032524-create-usuarios.js
@@ -2,15 +2,58 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    
+    return queryInterface.createTable("usuarios",{
+      id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      nome:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      senha:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      isLogged:{
+        type: Sequelize.BOOLEAN,
+        allowNull: true,
+      },
+      nivel:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      xp:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      xp_para_subir_de_nivel:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      foto:{
+        type: Sequelize.BLOB,
+        allowNull: true
+      },
+      doador:{
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      quant_exercicios_feitos:{
+        type: Sequelize.INTEGER,
+        allowNull: false
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return queryInterface.dropTable('usuarios');
+
   }
 };
